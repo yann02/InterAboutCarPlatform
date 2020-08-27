2c4c366f16e0928ef5f6b43d14f6ae14bbbf0cd3
yingyanwu <1179858356@qq.com>
2020-08-27 08:56:55 +0800

    添加请求注解说明和修改gradle配置文件

--- app/src/main/java/com/hnsh/dialogue/jet/network/ApiService.kt
+++ app/src/main/java/com/hnsh/dialogue/jet/network/ApiService.kt
@@ -16,6 +16,9 @@ interface ApiService {
     @POST(Constants.URL_MAIN_MENU_DATA)
     suspend fun searchPlaces(@FieldMap params: Map<String, Any>): HttpBaseBean
 
+    /**
+     * @JvmSuppressWildcards @FormUrlEncoded 两注解是为了让kotlin的Any类型在转Java的object类型时能正常转换
+     */
     @JvmSuppressWildcards
     @FormUrlEncoded
     @POST(Constants.URL_GET_APK_INFO)
